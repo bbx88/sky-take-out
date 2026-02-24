ff067a7aba775f16cb05cd1e09015c814ff007ee
Bobo <1044708998@qq.com>
2026-02-24 15:11:21 +0800

    支付功能代码开发

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -9,15 +9,27 @@ public interface UserMapper {
 
     /**
      * 根据openid查询用户
+     *
      * @param openid
      * @return
      */
     @Select("select * from user where openid=#{openid}")
     User getByOpenId(String openid);
 
-     /**
+    /**
      * 插入用户
+     *
      * @param user
      */
     void insert(User user);
+
+
+    /**
+     * 根据用户id查询用户
+     *
+     * @param userId
+     * @return
+     */
+    @Select("select * from user where id=#{userId}")
+    User getById(Long userId);
 }
